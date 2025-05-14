4f1b0e6ca19f1de591c9633fe93ca8444c87e720
HexKernel <187540092+HexKernel@users.noreply.github.com>
2025-05-14 18:37:49 +0330

    Update main2.cpp

--- test/main2.cpp
+++ test/main2.cpp
@@ -1,1 +1,7 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
 
+#define MODEM_RX 16
+#define MODEM_TX 17
+#define MODEM_BAUD 9600
+#define PC_BAUD 115200  // Default baud rate for laptop serial
