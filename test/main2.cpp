8a556529eec971568e76c4645aed02f247be7912
HexKernel <187540092+HexKernel@users.noreply.github.com>
2025-05-14 18:38:13 +0330

    Update main2.cpp

--- test/main2.cpp
+++ test/main2.cpp
@@ -5,3 +5,12 @@
 #define MODEM_TX 17
 #define MODEM_BAUD 9600
 #define PC_BAUD 115200  // Default baud rate for laptop serial
+
+SoftwareSerial sim800Serial(MODEM_RX, MODEM_TX);
+
+void setup() {
+  Serial.begin(PC_BAUD); // Set to 115200 for laptop
+  sim800Serial.begin(MODEM_BAUD); // 9600 for SIM800L
+  delay(3000);
+  Serial.println("SIM800L Serial Bridge Ready.");
+}
