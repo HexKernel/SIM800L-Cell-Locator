1ba2e283b78bd480918f2d71a44daa545555df91
HexKernel <187540092+HexKernel@users.noreply.github.com>
2025-05-18 01:48:57 +0330

    Update main2.cpp

--- test/main2.cpp
+++ test/main2.cpp
@@ -23,18 +23,17 @@
  * - Open a serial monitor at 115200 baud to interact with the SIM800L.
  */
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
 #define MODEM_RX 16
 #define MODEM_TX 17
 #define MODEM_BAUD 9600
 #define PC_BAUD 115200  // Default baud rate for laptop serial
 
-SoftwareSerial sim800Serial(MODEM_RX, MODEM_TX);
+HardwareSerial sim800Serial(2); // Use UART2 for SIM800L
 
 void setup() {
   Serial.begin(PC_BAUD); // Set to 115200 for laptop
-  sim800Serial.begin(MODEM_BAUD); // 9600 for SIM800L
+  sim800Serial.begin(MODEM_BAUD, SERIAL_8N1, MODEM_RX, MODEM_TX); // 9600 for SIM800L
   delay(3000);
   Serial.println("SIM800L Serial Bridge Ready.");
 }
