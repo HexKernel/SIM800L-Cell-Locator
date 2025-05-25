f5673a7dc517e8cfcb8584f039cfe1605e5a97e4
HexKernel <187540092+HexKernel@users.noreply.github.com>
2025-05-25 23:59:47 +0330

    Update main3.cpp

--- test/main3.cpp
+++ test/main3.cpp
@@ -27,12 +27,11 @@
  *
  * @return true if all required information was successfully retrieved and parsed, false otherwise.
  */
+#include <Arduino.h>
+#include <math.h>
+#include <map>
 
 
-// SIM800L pins and baud
-#define MODEM_RX 16
-#define MODEM_TX 17
-#define MODEM_BAUD 9600
 
 HardwareSerial sim800Serial(2); // Use UART2
 
